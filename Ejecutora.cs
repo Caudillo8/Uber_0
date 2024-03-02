eda009564195eb489790e6ef9af1ff38f338fe6b
hernandez matias <40743114+Caudillo8@users.noreply.github.com>
2024-03-02 16:37:17 -0300

    Add files via upload

---
+++ Ejecutora.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uber_HernandezMatias
+{
+    class Ejecutora
+    {
+        static void Main(string[] args)
+        {
+            const int cantViajes = 10;
+            Viaje[] Listado = new Viaje[cantViajes];
+
+            Ingreso(Listado);
+            Imprimir(Listado);
+            darMasCaro(Listado);
+
+            Console.WriteLine("\n\n<fin>");
+            Console.ReadKey();
+        }
+        public static void Ingreso(Viaje[]lista)
+        {
+            Console.WriteLine("\nING PRECIO POR KM\n");
+            float auxPrecioKm = float.Parse(Console.ReadLine());
+            Console.WriteLine("\nING KM minimo\n");
+            float auxMinKm = float.Parse(Console.ReadLine());
+            Viaje.costoKM = auxPrecioKm;
+            Viaje.minKm = auxMinKm;
+
+            for (int i=0; i < lista.Length; i++)
+            {
+
+                Console.WriteLine("\n\nING DOMINIO\n");
+                string auxDominio = Console.ReadLine();
+                Console.WriteLine("\nING DISTANCIA\n");
+                float auxDistancia = float.Parse(Console.ReadLine());
+
+                lista[i] = new Viaje(auxDominio, auxDistancia);
+                
+               
+            }
+        }
+        public static void Imprimir(Viaje[]listado)
+        {
+            Console.WriteLine("\n\nIMPRESION\n");
+            foreach(Viaje v in listado)
+            {
+                Console.WriteLine(v.DarDatos());
+            }
+        }
+        public static void darMasCaro(Viaje[] listado)
+        {
+            Viaje Caro = listado[0];
+            foreach(Viaje v in listado)
+            {
+                if (Caro.Comparar(v)) Caro = v;//si es verdadero v es mas caro
+            }
+            Console.WriteLine("\n\nVIAJE MAS CARO=\t"+Caro.DarDatos());
+        }
+    }
+}
---
+++ Viaje.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uber_HernandezMatias
+{
+    class Viaje
+    {
+
+        private string Dominio;
+        private float Distancia;
+
+        public Viaje()
+        {
+            this.Dominio = "SIN DATOS";
+            this.Distancia = 0;
+        }
+        public Viaje(string domi, float dist)
+        {
+            this.Dominio = domi;
+            this.Distancia = dist;
+        }
+        public  static float costoKM { get; set; }
+        public static float minKm { get; set; }
+
+        public string getDominio() { return this.Dominio; }
+        public void setDominio(string domi)
+        {
+            this.Dominio = domi;
+        }
+        public float getDistancia() { return this.Distancia; }
+        public void setDistancia(float dist) { this.Distancia = dist; }
+
+
+        public float Preciofinal()
+        {
+            float total;
+            if (this.getDistancia() < minKm)
+            {
+                this.setDistancia(minKm);
+            }
+            total = this.getDistancia() * costoKM;
+            return total;
+        }
+
+        public string DarDatos()
+        {
+            string datos;
+            datos = "\nDOMINIO\t" + this.getDominio();
+            datos += "\nDISTANCIA\t" + this.getDistancia();
+            datos += "\nMONTO FINAL\t" + this.Preciofinal();
+            return datos;
+        }
+        public bool Comparar(Viaje otro)//mas caro
+        {
+            if(this.Preciofinal() < otro.Preciofinal() )
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
